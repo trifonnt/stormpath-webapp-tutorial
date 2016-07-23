37b9882f4f9d0eeac1a22431f74819ea10344079
Trifon Trifonov <trifont@gmail.com>
2016-07-23 13:39:28 +0300

    Initial wokring version.

---
+++ src/main/java/tutorial/HomeController.java
@@ -0,0 +1,20 @@
+package tutorial;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class HomeController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final String VIEW_TEMPLATE_PATH = "/WEB-INF/jsp/home.jsp";
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getRequestDispatcher(VIEW_TEMPLATE_PATH).forward(req, resp);
+	}
+}
